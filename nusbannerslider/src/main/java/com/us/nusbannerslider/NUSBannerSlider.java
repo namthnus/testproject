dd7ec05c9600e2cea083e034dd3e8d1f14b87f87
David <david@nustechnology.com>
2017-07-24 15:05:31 +0700

    init commit

---
+++ nusbannerslider/src/main/java/com/us/nusbannerslider/NUSBannerSlider.java
@@ -0,0 +1,28 @@
+package com.us.nusbannerslider;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import ss.com.bannerslider.views.BannerSlider;
+
+/**
+ * Created by nus on 7/24/17.
+ */
+
+public class NUSBannerSlider extends BannerSlider {
+    public NUSBannerSlider(Context context) {
+        super(context);
+    }
+
+    public NUSBannerSlider(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NUSBannerSlider(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public NUSBannerSlider(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
